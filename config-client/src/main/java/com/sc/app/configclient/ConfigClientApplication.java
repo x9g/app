018b6d46343f9cd82828f08f8164ceab8a8b4ace
chrisred8 <chris@hiredbull.com>
2019-05-30 18:56:14 +0800

    bus

--- config-client/src/main/java/com/sc/app/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/sc/app/configclient/ConfigClientApplication.java
@@ -1,18 +1,20 @@
-package com.sc.app.feignservice;
+package com.sc.app.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
+@EnableDiscoveryClient
+@RefreshScope
 @Configuration
-public class ServiceFeignApplication {
+public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceFeignApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
     }
 }
---
+++ config-server/src/main/java/com/sc/app/configserver/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.sc.app.configserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableConfigServer
+@Configuration
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
